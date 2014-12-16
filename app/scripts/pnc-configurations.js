9e917e871f040d6fc6724a719dd3c61634e98efa
Andrea Vibelli <avibelli@redhat.com>
2014-12-16 18:20:16 +0100

    New PR after merge conflicts

--- app/scripts/pnc-configurations.js
+++ app/scripts/pnc-configurations.js
@@ -23,8 +23,6 @@ $(document).ready(function() {
       { 'data': 'buildScript' },
       { 'data': 'scmUrl' },
       { 'data': 'patchesUrl' },
-      { 'data': 'creationTime' },
-      { 'data': 'lastModificationTime' },                    
       { 'data':
         function(json) {
           return '<button class="build btn btn-block btn-danger" value="' + json.id + '">Build</button>';
@@ -33,15 +31,64 @@ $(document).ready(function() {
     ]
   });
   
-  $('#products tbody').on( 'click', 'button.build', function (event) {
+  $('#configuration_content tbody').on( 'click', 'button.build', function (event) {
     event.preventDefault();
     $.post(PNC_REST_BASE_URL + 'configuration/' + $(this).attr('value') + '/build').
         done(function() {
-          $('#content').prepend('<br/><div class="alert alert-success" role="alert">Build successfully triggered</div>');
+          $('#configuration_content').prepend('<br/><div class="alert alert-success" role="alert">Build successfully triggered</div>');
           console.log('success');
         }).fail(function() {
-          $('#content').prepend('<br/><div class="alert alert-danger" role="alert">Error attempting to trigger build</div>');
+          $('#configuration_content').prepend('<br/><div class="alert alert-danger" role="alert">Error attempting to trigger build</div>');
           console.log('failure');
         });
   });
+
+  $('#configuration_content').on( 'click', 'button.addConfiguration', function (event) {
+    event.preventDefault();
+    $(location).attr('href',"configuration_add.html");
+  });
+
+  $('#configuration_content_add').on( 'click', 'button.cancelConfiguration', function (event) {
+    event.preventDefault();
+    $(location).attr('href',"configurations.html");
+  });
+
+  $('#configuration_content_add').on( 'click', 'button.saveConfiguration', function (event) {
+
+       event.preventDefault();
+
+       var identifier = $('#addConfIdentifier').val();
+       var buildScript = $('#addConfBuildScript').val();
+       var scmUrl = $('#addConfScmUrl').val();
+       var patchesUrl = $('#addConfPatchesUrl').val();
+
+       var JSONObj = {
+            "identifier": identifier,
+            "buildScript": buildScript,
+            "scmUrl": scmUrl,
+            "patchesUrl": patchesUrl
+       };
+
+       var data = JSON.stringify(JSONObj);
+       console.log('Creating new build configuration: ' + data);
+
+       $.ajax({
+               url: PNC_REST_BASE_URL + 'product/' + productId + '/version/' + versionId + '/project/' + projectId + '/configuration',
+               type: 'POST',
+               dataType : 'json',
+               data: data,
+               contentType: "application/json; charset=utf-8",
+               success: function(data) {
+                  console.log('build configuration creation was successful');
+               },
+               failure: function(errMsg) {
+                 console.log('build configuration creation was NOT successful: ' + errMsg);
+               },
+               complete: function(xhr, status) {
+                  console.log('build configuration creation complete!!!!');
+                  $(location).attr('href',"configurations.html");
+               }
+       });
+  });
+
 } );
