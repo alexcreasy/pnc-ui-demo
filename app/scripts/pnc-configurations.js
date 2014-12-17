09b2f32dab350d65f74ced352df9774c4af49816
Alex Creasy <acreasy@redhat.com>
2014-12-17 23:28:13 +0000

    Polling server for build completion and provide link to results

--- app/scripts/pnc-configurations.js
+++ app/scripts/pnc-configurations.js
@@ -1,6 +1,10 @@
 // Initialize Datatables
 $(document).ready(function() {
 
+  // CONSTANT VALUES
+  var MAX_POLLS = 200
+  var POLL_INTERVAL = 10000 //ms
+
   // Clear all sessionStorage except productId,versionId and projectId
   var product = $.parseJSON(sessionStorage.getItem('product'));
   var version = $.parseJSON(sessionStorage.getItem('version'));
@@ -49,22 +53,90 @@ $(document).ready(function() {
       },
       { 'data':
         function(json) {
-          return '<button class="build btn btn-block btn-danger" data-configuration-id="' + json.id + '">Build</button>';
+          return '<button class="build btn btn-block btn-danger" id="btn-trigger-build-' + json.id + '" data-configuration-id="' + json.id + '">Build</button>';
         }
       }
     ]
   });
-  
+
+  /*
+   *
+   * Triggering a build
+   *
+   */
+
+  function postTriggerBuild(configId, pollUrl) {
+    console.log('postTriggerBuild()');
+    $('#btn-trigger-build-' + configId).parents('td').html(
+      '<p><span id="in-progress-build-' + configId + '" class="spinner spinner-xs spinner-inline"></span> Building</p>');
+
+    // poll counter
+    var polls = 0;
+    // Self executing polling function
+    (function poll(){
+      setTimeout(
+        function(){
+          console.log('SetTimeOutFunction()');
+          $.ajax(
+            { url: pollUrl, 
+              complete: 
+                function(data, textStatus) {
+                  if (polls > MAX_POLLS) {
+                    throw new Error('Maximum number of polls exceeded');
+                  }
+
+                  console.log('Poll #%d Result: data={%O}, textStatus{%O}', poll++, data, textStatus);
+
+                  // Action the result of the poll
+                  switch (data.status) {
+                    case 200:
+                      if (data.responseJSON.status === 'BUILDING') {
+                        console.log('BUILD IN PROGRESS');
+                        // Contiune polling
+                        poll();
+                      } else {
+                        throw new Error('HTTP response 200 but JSON status other than BUILDING returned');
+                      }
+                      break;
+                    case 204:
+                      console.log('BUILD COMPLETED');
+                      buildCompleted(configId, pollUrl);
+                      break;
+                    default:
+                      throw new Error('Unrecognised HTTP response received: ' + data.responseJSON.status);
+                  }
+                } 
+            }
+          );
+        }, 
+        POLL_INTERVAL
+      );
+    })();
+  }
+
+  function buildCompleted(configId, pollUrl) {
+    console.log('buildCompleted()');
+    sessionStorage.setItem('configId', configId);
+    $('#in-progress-build-' + configId).parents('td').html('<a class="btn btn-success" href="views/results.html">COMPLETED</a>');
+    $('#alert-space').empty();
+  }
+
+
   $('#configuration tbody').on( 'click', 'button.build', 
     function (event) {
       event.preventDefault();
       console.log('trigger build click registered');
-      $.post(PNC_REST_BASE_URL + '/product/' + product.id + '/version/' + version.id + '/project/' + project.id +
-             '/configuration/' + $(this).data("configuration-id") + '/build')
+
+      var configId = $(this).data("configuration-id");
+
+      $.post(PNC_REST_BASE_URL + '/product/' + sessionStorage.getItem('productId') + '/version/' + 
+             sessionStorage.getItem('versionId') + '/project/' + sessionStorage.getItem('projectId') + 
+             '/configuration/' + configId + '/build')
         .done(
           function(data, text, xhr) {
             $('#alert-space').prepend('<br/><div class="alert alert-success" role="alert">Build successfully triggered</div>');
             console.log('Trigger build successful: data={%O}, text={%O}, xhr={%O}', data, text, xhr);
+            postTriggerBuild(configId, data);
           }
         )
         .fail(
@@ -75,6 +147,14 @@ $(document).ready(function() {
         );
     }
   );
+
+
+  /*
+   *
+   * Creating new configuration
+   *
+   */
+
 
   $('#configuration_content').on( 'click', 'button.addConfiguration', function (event) {
     event.preventDefault();
